976da7ea90cbc7a5a8aa75f9cef66d98f1565ec1
Rachel <rjcrter11@gmail.com>
2020-07-10 12:27:31 -0500

    completes ex5

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,20 @@
 # Your code here
 
 
-
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+
+    cache = {}
+    result = []
+
+    for query in queries:
+        if query not in cache:
+            cache[query] = query
+
+    for file in files:
+        file_name = file.split('/')[-1]
+
+        if file_name in cache:
+            result.append(file)
 
     return result
 

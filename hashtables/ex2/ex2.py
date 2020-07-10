9753b651b6721915ea3382d75eff30095fb09bf0
Rachel <rjcrter11@gmail.com>
2020-07-10 11:01:22 -0500

    completes ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,24 @@ class Ticket:
         self.source = source
         self.destination = destination
 
+    def __str__(self):
+        return f'Source: {self.source} - Destination: {self.destination}'
+
 
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+
+    trip_cache = {}
+    route = []
+
+    for ticket in tickets:
+        trip_cache[ticket.source] = ticket.destination
+
+    next_destination = trip_cache['NONE']
+
+    while next_destination != 'NONE':
+        route.append(next_destination)
+        next_destination = trip_cache[next_destination]
+    route.append('NONE')
+    print(route)
 
     return route

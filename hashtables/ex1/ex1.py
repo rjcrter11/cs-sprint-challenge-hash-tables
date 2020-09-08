17899b143b4ca91de0158879c748dbffc3dc0d65
Rachel <rjcrter11@gmail.com>
2020-09-08 14:20:05 -0500

    commit

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,11 @@
 
+
 def get_indices_of_item_weights(weights, length, limit):
 
     weights_dict = {}
 
     for i in range(len(weights)):
+
         weight = weights[i]
         if weight in weights_dict:
             cached_weight = weights_dict[weight]
